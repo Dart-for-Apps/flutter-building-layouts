6e71c4753e0f73e2a7ad3cf73200ecdd786bed50
wurikiji <wurikiji@gmail.com>
2018-12-25 20:06:37 +0900

    Complete Step 4

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,14 @@ class BuildingLayouts extends StatelessWidget {
         title: Text('Building Lyaouts'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           TitleSection(),
           SizedBox(
-            height: 8.0,
+            height: 9.0,
           ),
           ButtonSection(),
+          TextSection(),
         ],
       ),
     );
@@ -117,6 +119,22 @@ class ButtonSection extends StatelessWidget {
         CustomButton(icon: Icons.near_me, label: 'CALL'),
         CustomButton(icon: Icons.share, label: 'CALL'),
       ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.all(32.0),
+      child: Text(
+        '''
+아무 텍스트나 길게 입력 하면 됩니다. 하지만 나는 왼쪽 정렬을 하고 싶지. 크기도 고정하고 싶지 아무 텍스트나 길게 입력 하면 됩니다. 하지만 나는 왼쪽 정렬을 하고 싶지. 크기도 고정하고 싶지 아무 텍스트나 길게 입력 하면 됩니다. 하지만 나는 왼쪽 정렬을 하고 싶지. 크기도 고정하고 싶지 아무 텍스트나 길게 입력 하면 됩니다. 하지만 나는 왼쪽 정렬을 하고 싶지. 크기도 고정하고 싶지 
+          ''',
+        softWrap: true,
+      ),
     );
   }
 }
