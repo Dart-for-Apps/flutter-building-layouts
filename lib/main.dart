2efbc161ae7797d01b774174b5faa77e5f08c10a
wurikiji <wurikiji@gmail.com>
2018-12-25 19:47:02 +0900

    Complete Step 3

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Building Layouts',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,6 +27,10 @@ class BuildingLayouts extends StatelessWidget {
       body: Column(
         children: <Widget>[
           TitleSection(),
+          SizedBox(
+            height: 8.0,
+          ),
+          ButtonSection(),
         ],
       ),
     );
@@ -70,6 +74,49 @@ class TitleSection extends StatelessWidget {
           Text('41'),
         ],
       ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  CustomButton({this.icon, this.label});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    Color color = Theme.of(context).primaryColor;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Icon(icon, color: color),
+        SizedBox(
+          height: 8.0,
+        ),
+        Text(label,
+            style: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ))
+      ],
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        CustomButton(icon: Icons.call, label: 'CALL'),
+        CustomButton(icon: Icons.near_me, label: 'CALL'),
+        CustomButton(icon: Icons.share, label: 'CALL'),
+      ],
     );
   }
 }
